3116b561efd40f08f20642ca66e3c04480e405ae
yuhongchun <yuhongchun027@gmail.com>
2016-03-05 18:33:58 +0800

    Version 2.0

---
+++ 《Linux集群和自动化运维》第7章/7.3.1/realserver.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+SNS_VIP=192.168.1.210
+. /etc/rc.d/init.d/functions
+
+case "$1" in
+start)
+ifconfig lo:0 $SNS_VIP netmask 255.255.255.255 broadcast $SNS_VIP
+/sbin/route add -host $SNS_VIP dev lo:0
+echo "1" >/proc/sys/net/ipv4/conf/lo/arp_ignore
+echo "2" >/proc/sys/net/ipv4/conf/lo/arp_announce
+echo "1" >/proc/sys/net/ipv4/conf/all/arp_ignore
+echo "2" >/proc/sys/net/ipv4/conf/all/arp_announce
+sysctl -p >/dev/null 2>&1
+echo "RealServer Start OK"
+;;
+stop)
+ifconfig lo:0 down
+route del $LVS_VIP >/dev/null 2>&1
+echo "0" >/proc/sys/net/ipv4/conf/lo/arp_ignore
+echo "0" >/proc/sys/net/ipv4/conf/lo/arp_announce
+echo "0" >/proc/sys/net/ipv4/conf/all/arp_ignore
+echo "0" >/proc/sys/net/ipv4/conf/all/arp_announce
+echo "RealServer Stoped"
+;;
+*)
+echo "Usage: $0 {start|stop}"
+exit 1
+esac
+exit 0
