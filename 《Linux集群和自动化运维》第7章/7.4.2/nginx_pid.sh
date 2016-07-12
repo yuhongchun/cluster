eed244da2d01f796ed353a887082927f083cd3b1
yuhongchun <yuhongchun027@gmail.com>
2016-07-12 19:50:33 +0800

    fix nginx_pid.sh

--- 《Linux集群和自动化运维》第7章/7.4.2/nginx_pid.sh
+++ 《Linux集群和自动化运维》第7章/7.4.2/nginx_pid.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-while  :
+while :
 do
- nginxpid=`ps -C nginx --no-header | wc -l`
- if [ $nginxpid -eq 0 ];then
-  /usr/local/nginx/sbin/nginx
-  sleep 5
-   if [ $nginxpid -eq 0 ];then
-   /etc/init.d/keepalived stop
-   fi
- fi
- sleep 5
+	nginxpid=`ps -C nginx --no-header | wc -l`
+ 	if [ $nginxpid -eq 0 ];then
+  		/usr/local/nginx/sbin/nginx
+  		sleep 5
+   			if [ $nginxpid -eq 0 ];then
+   				/etc/init.d/keepalived stop
+   			fi
+ 	fi
+ 	sleep 5
 done
